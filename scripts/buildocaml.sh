534e7eb2e15410f6784b3a84f94b25f258b141df
Benjamin San Souci <benjamin.sansouci@gmail.com>
2017-08-22 13:42:22 -0700

    Testing allowing Dynlink in native.

--- scripts/buildocaml.sh
+++ scripts/buildocaml.sh
@@ -4,6 +4,6 @@ set -e
 
 # export OCAMLPARAM='_,bin-annot=1'
 # export OCAMLRUNPARAM=b
-
+# -no-shared-libs
 cd vendor/ocaml
-./configure -prefix `pwd`  -no-ocamldoc -no-ocamlbuild -no-shared-libs -no-curses -no-graph -no-pthread -no-debugger  && make -j9 world.opt && make install  && make -C stdlib install && mkdir -p "$(dirname `pwd`)"/lib/ocaml/caml && cp otherlibs/systhreads/threads.h "$(dirname `pwd`)"/lib/ocaml/caml/threads.h && make -C otherlibs/systhreads && cd ..
+./configure -prefix `pwd`  -no-ocamldoc -no-ocamlbuild -no-curses -no-graph -no-pthread -no-debugger  && make -j9 world.opt && make install  && make -C stdlib install && mkdir -p "$(dirname `pwd`)"/lib/ocaml/caml && cp otherlibs/systhreads/threads.h "$(dirname `pwd`)"/lib/ocaml/caml/threads.h && make -C otherlibs/systhreads && cd ..
