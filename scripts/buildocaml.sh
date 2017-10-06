2d2a7e81a7c6a51f7462ef7a6750104c47b1c0e3
Benjamin San Souci <benjamin.sansouci@gmail.com>
2017-10-06 13:37:33 -0700

    Clean up install script

--- scripts/buildocaml.sh
+++ scripts/buildocaml.sh
@@ -7,4 +7,4 @@ set -e
 # -no-shared-libs
 DIRNAME="$(dirname `pwd`)"
 cd vendor/ocaml
-./configure -prefix `pwd`  -no-ocamldoc -no-ocamlbuild -no-curses -no-graph -no-pthread -no-debugger  && make -j9 world.opt && make install  && make -C stdlib install && mkdir -p $DIRNAME/lib/ocaml/caml && cp otherlibs/systhreads/threads.h $DIRNAME/lib/ocaml/caml/threads.h && make -C otherlibs/systhreads && cd ..
+./configure -prefix `pwd`  -no-ocamldoc -no-ocamlbuild -no-curses -no-graph -no-pthread -no-debugger  && make -j9 world.opt && make install && mkdir -p $DIRNAME/lib/ocaml/caml && make -C otherlibs/systhreads && cp otherlibs/systhreads/threads.h $DIRNAME/lib/ocaml/caml/threads.h && cd ..
