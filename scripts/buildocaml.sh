657374d5866e0b9bab9318c71325c05d00473e21
Benjamin San Souci <benjamin.sansouci@gmail.com>
2017-10-06 13:41:08 -0700

    Derp derp derp

--- scripts/buildocaml.sh
+++ scripts/buildocaml.sh
@@ -5,6 +5,6 @@ set -e
 # export OCAMLPARAM='_,bin-annot=1'
 # export OCAMLRUNPARAM=b
 # -no-shared-libs
-DIRNAME="$(dirname `pwd`)"
+DIRNAME=`pwd`
 cd vendor/ocaml
 ./configure -prefix `pwd`  -no-ocamldoc -no-ocamlbuild -no-curses -no-graph -no-pthread -no-debugger  && make -j9 world.opt && make install && mkdir -p $DIRNAME/lib/ocaml/caml && make -C otherlibs/systhreads && cp otherlibs/systhreads/threads.h $DIRNAME/lib/ocaml/caml/threads.h && cd ..
