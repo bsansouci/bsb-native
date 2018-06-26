7783ffa417f7b3d074f7590b17ba094fd2127926
Avery Morin <avery.schmavery@gmail.com>
2018-06-26 14:57:42 -0700

    Use write-output

--- msys2_32.ps1
+++ msys2_32.ps1
@@ -1,23 +1,23 @@
-Write-Host "Installing MSYS2 32-bit..." -ForegroundColor Cyan
+Write-Output "Installing MSYS2 32-bit..." -ForegroundColor Cyan
 
 # download installer
 $zipPath = "$($env:USERPROFILE)\msys2-i686-latest.tar.xz"
 $tarPath = "$($env:USERPROFILE)\msys2-i686-latest.tar"
-Write-Host "Downloading MSYS installation package..."
+Write-Output "Downloading MSYS installation package..."
 (New-Object Net.WebClient).DownloadFile('http://repo.msys2.org/distrib/msys2-i686-latest.tar.xz', $zipPath)
 
-Write-Host "Untaring installation package..."
+Write-Output "Untaring installation package..."
 7z x $zipPath -y -o"$env:USERPROFILE" | Out-Null
 
-Write-Host "Unzipping installation package..."
+Write-Output "Unzipping installation package..."
 7z x $tarPath -y -oC:\ | Out-Null
 del $zipPath
 del $tarPath
 
 function bash($command) {
-    Write-Host $command -NoNewline
+    Write-Output $command -NoNewline
     cmd /c start /wait C:\msys32\usr\bin\sh.exe --login -c $command
-    Write-Host " - OK" -ForegroundColor Green
+    Write-Output " - OK" -ForegroundColor Green
 }
 
 [Environment]::SetEnvironmentVariable("MSYS2_PATH_TYPE", "inherit", "Machine")
@@ -34,5 +34,4 @@ bash 'pacman --sync --noconfirm base-devel'
 bash 'pacman --sync --noconfirm msys2-devel'
 bash 'pacman --sync --noconfirm mingw-w64-{x86_64,i686}-toolchain'
 
-Write-Host "MSYS2 32-bit installed" -ForegroundColor Green
-Stop-Process $pid
+Write-Output "MSYS2 32-bit installed" -ForegroundColor Green
