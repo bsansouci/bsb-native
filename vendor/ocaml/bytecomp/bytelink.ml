a6476c8af186f2c05362c6f8c096a7d5b1c60467
Benjamin San Souci <benjamin.sansouci@gmail.com>
2018-06-25 22:41:51 -0700

    Fix running bytecode on unix/windows.
    For windows we use a camlheader that's a binary written in C. For linux
    we simply use #! with a path to the interpreter.

--- vendor/ocaml/bytecomp/bytelink.ml
+++ vendor/ocaml/bytecomp/bytelink.ml
@@ -306,12 +306,16 @@ let link_bytecode ppf tolink exec_name standalone =
     if standalone then begin
       (* Copy the header *)
       try
-        let header =
-          if String.length !Clflags.use_runtime > 0
-          then "camlheader_ur" else "camlheader" ^ !Clflags.runtime_variant in
-        let inchan = open_in_bin (find_in_path !load_path header) in
-        copy_file inchan outchan;
-        close_in inchan
+        begin match Sys.os_type with
+          "Win32" | "Cygwin" ->
+            let header = if String.length !Clflags.use_runtime > 0 then "camlheader_ur"  else "camlheader" ^ !Clflags.runtime_variant in
+            let inchan = open_in_bin (find_in_path !load_path header) in
+            copy_file inchan outchan;
+            close_in inchan
+        | _ ->
+          output_string outchan ("#!" ^ (make_absolute standard_runtime));
+          output_char outchan '\n';
+        end
       with Not_found | Sys_error _ -> ()
     end;
     Bytesections.init_record outchan;
